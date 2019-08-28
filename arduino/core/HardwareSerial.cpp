c54b568d7f789a1df5ec7444573dfebad2fe3ea4
Georgi Angelov <the.wizarda@gmail.com>
2019-08-28 11:28:40 +0300

    available()

--- arduino/core/HardwareSerial.cpp
+++ arduino/core/HardwareSerial.cpp
@@ -19,7 +19,7 @@
 
 #include "HardwareSerial.h"
 
-HardwareSerial::HardwareSerial(int port_id) : id(port_id), fd(-1), ring_buffer(0), ring_count(0){};
+HardwareSerial::HardwareSerial(int port_id) : id(port_id), fd(-1), peeked(0) {};
 
 void HardwareSerial::begin(UART_Config *pUartConfig)
 {
@@ -38,78 +38,50 @@ void HardwareSerial::begin(unsigned long baudrate)
 
 size_t HardwareSerial::write(uint8_t c)
 {
-    if (fd < 0)
-        return 0;
-    ssize_t sent = ::write(fd, &c, 1);
-    return (sent == 1) ? 1 : 0;
-}
-
-char HardwareSerial::get()
-{
-    char data = ring_buffer & 0xFF;
-    ring_buffer >>= 8;
-    ring_count--;
-    return data;
-}
-
-int HardwareSerial::read(char *buf, int size)
-{
-#define MIN(a, b) ((a) < (b) ? a : b)
-    if (fd < 0 || NULL == buf || size < 1)
-        return -1;
-    int rd = 0;
-    if (ring_count > 0)
-    {
-        int m = MIN(size, ring_count);
-        while (m > 0)
-        {
-            *buf++ = get();
-            size--;
-            rd++;
-            m--;
-        }
-    }
-    if (size)
-    {
-        int cnt = ::read(fd, buf, size);
-        if (cnt > 0)
-            rd += cnt;
-    }
-    return (rd) ? rd : -1;
+    int w = ::write(fd, &c, 1);
+    return w == 1 ? w : 0;
 }
 
 int HardwareSerial::read(void)
 {
-    char data;
-    return (1 == this->read(&data, 1)) ? data : 0;
+    if (peeked)
+    {
+        peeked = 0;
+        return pk;
+    }
+    char b;
+    return ::read(fd, &b, 1) == 1 ? b : -1;
 }
 
-/// NOT EXIST ioctl(serial_fd, FIONREAD, &bytes_avail);
 int HardwareSerial::available(void)
 {
     if (fd < 0)
         return 0;
-    if (ring_count > 0)
-        return ring_count;
-    ring_count = ::read(fd, &ring_buffer, sizeof(ring_buffer)); /* max 8 bytes */
-    return (ring_count > 0) ? ring_count : 0;
+    int cnt = -1;
+    return (0 == api_ioctl(fd, FIONREAD, (uint32_t)&cnt)) ? cnt : 0;
 }
 
 int HardwareSerial::peek(void)
 {
-    if (available())
+    if (fd == -1 || !available())
+        return -1;
+    if (peeked)
     {
-        if (ring_count > 0)
-            return ring_buffer & 0xFF; // get ring_char[0]
+        return pk;
+    }
+    else
+    {
+        pk = read();
+        if (pk != -1)
+        {
+            peeked = 1;
+            return pk;
+        }
     }
     return -1;
 }
 
 void HardwareSerial::flush(void)
 {
-    ring_buffer = 0;
-    ring_count = 0;
-    while (read() > 0)
-    {
-    }
+    api_ioctl(fd, TCFLSH, 2); // flush both
 };
