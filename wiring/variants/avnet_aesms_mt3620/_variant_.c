1963ab0d20940c065853338c32e7dfc9c75cf3fc
Georgi Angelov <the.wizarda@gmail.com>
2019-09-30 15:41:01 +0300

    Update _variant_.c

--- wiring/variants/avnet_aesms_mt3620/_variant_.c
+++ wiring/variants/avnet_aesms_mt3620/_variant_.c
@@ -4,13 +4,37 @@
 #include <mt3620-baremetal.h>
 #include <mt3620-gpio.h>
 
-// Block includes led1RedGpio, GPIO8.
-static const GpioBlock pwm2 = {
-    .baseAddr = 0x38030000, .type = GpioBlock_PWM, .firstPin = 8, .pinCount = 4};
+/*
+  PWM  = GPIO_0  to GPIO_3  (0x38010000)
+  PWM  = GPIO_4  to GPIO_7  (0x38020000)
+  PWM  = GPIO_8  to GPIO_11 (0x38030000)
+  GRP3 = GPIO_12 to GPIO_15 (0x38040000)
+  GRP4 = GPIO_16 to GPIO_19 (0x38050000)
+  GRP5 = GPIO_20 to GPIO_23 (0x38060000)
+  ISU0 = GPIO_26 to GPIO_30 (0x38070000) 
+  ISU1 = GPIO_31 to GPIO_35 (0x38080000)
+  ISU2 = GPIO_36 to GPIO_40 (0x38090000) 
+  ISU3 = GPIO_66 to GPIO_70 (0x380A0000) 
+  ISU4 = GPIO_71 to GPIO_75 (0x380B0000) 
+  ADC  = GPIO_41 to GPIO_48 (0x38000000)
+    
+static const GpioBlock gpio_block_0 = { .baseAddr = 0x38000000, .type = GpioBlock_ADC, .firstPin = 41, .pinCount = 7 };    
+static const GpioBlock gpio_block_1 = { .baseAddr = 0x38010000, .type = GpioBlock_PWM, .firstPin =  0, .pinCount = 4 };
+static const GpioBlock gpio_block_2 = { .baseAddr = 0x38020000, .type = GpioBlock_PWM, .firstPin =  4, .pinCount = 3 };
+*/
 
-// Block includes buttonAGpio, GPIO12
-static const GpioBlock grp3 = {
-    .baseAddr = 0x38040000, .type = GpioBlock_GRP, .firstPin = 12, .pinCount = 4};
+static const GpioBlock pwm2 = { .baseAddr = 0x38030000, .type = GpioBlock_PWM, .firstPin =  8, .pinCount = 4 };
+static const GpioBlock grp3 = { .baseAddr = 0x38040000, .type = GpioBlock_GRP, .firstPin = 12, .pinCount = 4 };
+
+/*
+static const GpioBlock gpio_block_5 = { .baseAddr = 0x38050000, .type = GpioBlock_GRP, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_6 = { .baseAddr = 0x38060000, .type = GpioBlock_GRP, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_7 = { .baseAddr = 0x38070000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_8 = { .baseAddr = 0x38080000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_9 = { .baseAddr = 0x38090000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_A = { .baseAddr = 0x380A0000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_B = { .baseAddr = 0x380B0000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+*/
 
 void init(void)
 {
