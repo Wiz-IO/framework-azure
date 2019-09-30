6f61636e18a43b6c29df81cb627028a1dff77162
Georgi Angelov <the.wizarda@gmail.com>
2019-09-30 15:47:27 +0300

    Update _variant_.c

--- wiring/variants/avnet_aesms_mt3620/_variant_.c
+++ wiring/variants/avnet_aesms_mt3620/_variant_.c
@@ -22,22 +22,34 @@ static const GpioBlock gpio_block_0 = { .baseAddr = 0x38000000, .type = GpioBloc
 static const GpioBlock gpio_block_1 = { .baseAddr = 0x38010000, .type = GpioBlock_PWM, .firstPin =  0, .pinCount = 4 };
 static const GpioBlock gpio_block_2 = { .baseAddr = 0x38020000, .type = GpioBlock_PWM, .firstPin =  4, .pinCount = 3 };
 */
-
-static const GpioBlock pwm2 = { .baseAddr = 0x38030000, .type = GpioBlock_PWM, .firstPin =  8, .pinCount = 4 };
-static const GpioBlock grp3 = { .baseAddr = 0x38040000, .type = GpioBlock_GRP, .firstPin = 12, .pinCount = 4 };
-
+static const GpioBlock pwm2         = { .baseAddr = 0x38030000, .type = GpioBlock_PWM, .firstPin =  8, .pinCount = 4 };
+static const GpioBlock grp3         = { .baseAddr = 0x38040000, .type = GpioBlock_GRP, .firstPin = 12, .pinCount = 4 };
 /*
-static const GpioBlock gpio_block_5 = { .baseAddr = 0x38050000, .type = GpioBlock_GRP, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_6 = { .baseAddr = 0x38060000, .type = GpioBlock_GRP, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_7 = { .baseAddr = 0x38070000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_8 = { .baseAddr = 0x38080000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_9 = { .baseAddr = 0x38090000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_A = { .baseAddr = 0x380A0000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
-static const GpioBlock gpio_block_B = { .baseAddr = 0x380B0000, .type = GpioBlock_ISU, .firstPin =  , .pinCount =  };
+static const GpioBlock gpio_block_5 = { .baseAddr = 0x38050000, .type = GpioBlock_GRP, .firstPin = 16, .pinCount = 3 };
+static const GpioBlock gpio_block_6 = { .baseAddr = 0x38060000, .type = GpioBlock_GRP, .firstPin = 20, .pinCount = 4 };
+static const GpioBlock gpio_block_7 = { .baseAddr = 0x38070000, .type = GpioBlock_ISU, .firstPin = 26, .pinCount = 4 };
+static const GpioBlock gpio_block_8 = { .baseAddr = 0x38080000, .type = GpioBlock_ISU, .firstPin = 31, .pinCount = 4 };
+static const GpioBlock gpio_block_9 = { .baseAddr = 0x38090000, .type = GpioBlock_ISU, .firstPin = 36, .pinCount = 4 };
+static const GpioBlock gpio_block_A = { .baseAddr = 0x380A0000, .type = GpioBlock_ISU, .firstPin = 66, .pinCount = 4 };
+static const GpioBlock gpio_block_B = { .baseAddr = 0x380B0000, .type = GpioBlock_ISU, .firstPin = 71, .pinCount = 4 };
 */
 
 void init(void)
 {
-    Mt3620_Gpio_AddBlock(&pwm2);
-    Mt3620_Gpio_AddBlock(&grp3);
+/*
+   Mt3620_Gpio_AddBlock(&gpio_block_0);
+   Mt3620_Gpio_AddBlock(&gpio_block_1);
+   Mt3620_Gpio_AddBlock(&gpio_block_2);
+*/  
+    Mt3620_Gpio_AddBlock(&pwm2); // gpio_block_3
+    Mt3620_Gpio_AddBlock(&grp3); // gpio_block_4 
+/*
+    Mt3620_Gpio_AddBlock(&gpio_block_5);
+    Mt3620_Gpio_AddBlock(&gpio_block_6);
+    Mt3620_Gpio_AddBlock(&gpio_block_7);
+    Mt3620_Gpio_AddBlock(&gpio_block_8);
+    Mt3620_Gpio_AddBlock(&gpio_block_9);
+    Mt3620_Gpio_AddBlock(&gpio_block_A);
+    Mt3620_Gpio_AddBlock(&gpio_block_B);
+*/  
 }
