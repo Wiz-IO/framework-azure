bc16a232dc703b01dcfe9d365757f27d7e8f3c93
Georgi Angelov <the.wizarda@gmail.com>
2019-10-04 21:43:48 +0300

    add all gpio blocks

--- wiring/variants/avnet_aesms_mt3620/_variant_.c
+++ wiring/variants/avnet_aesms_mt3620/_variant_.c
@@ -19,14 +19,14 @@
     ISU4 = GPIO_71 to GPIO_75 (0x380B0000) 
 */
 
-/*    
+   
 static const GpioBlock gpio_block_0 = { .baseAddr = 0x38000000, .type = GpioBlock_ADC, .firstPin = 41, .pinCount = 8 };    
 static const GpioBlock gpio_block_1 = { .baseAddr = 0x38010000, .type = GpioBlock_PWM, .firstPin =  0, .pinCount = 4 };
 static const GpioBlock gpio_block_2 = { .baseAddr = 0x38020000, .type = GpioBlock_PWM, .firstPin =  4, .pinCount = 4 };
-*/
+
 static const GpioBlock pwm2         = { .baseAddr = 0x38030000, .type = GpioBlock_PWM, .firstPin =  8, .pinCount = 4 };
 static const GpioBlock grp3         = { .baseAddr = 0x38040000, .type = GpioBlock_GRP, .firstPin = 12, .pinCount = 4 };
-/*
+
 static const GpioBlock gpio_block_5 = { .baseAddr = 0x38050000, .type = GpioBlock_GRP, .firstPin = 16, .pinCount = 4 };
 static const GpioBlock gpio_block_6 = { .baseAddr = 0x38060000, .type = GpioBlock_GRP, .firstPin = 20, .pinCount = 4 };
 static const GpioBlock gpio_block_7 = { .baseAddr = 0x38070000, .type = GpioBlock_ISU, .firstPin = 26, .pinCount = 5 };
@@ -34,18 +34,18 @@ static const GpioBlock gpio_block_8 = { .baseAddr = 0x38080000, .type = GpioBloc
 static const GpioBlock gpio_block_9 = { .baseAddr = 0x38090000, .type = GpioBlock_ISU, .firstPin = 36, .pinCount = 5 };
 static const GpioBlock gpio_block_A = { .baseAddr = 0x380A0000, .type = GpioBlock_ISU, .firstPin = 66, .pinCount = 5 };
 static const GpioBlock gpio_block_B = { .baseAddr = 0x380B0000, .type = GpioBlock_ISU, .firstPin = 71, .pinCount = 5 };
-*/
+
 
 void init(void)
 {
-/*
-   Mt3620_Gpio_AddBlock(&gpio_block_0);
-   Mt3620_Gpio_AddBlock(&gpio_block_1);
-   Mt3620_Gpio_AddBlock(&gpio_block_2);
-*/  
+    
+    Mt3620_Gpio_AddBlock(&gpio_block_0);
+    Mt3620_Gpio_AddBlock(&gpio_block_1);
+    Mt3620_Gpio_AddBlock(&gpio_block_2);
+  
     Mt3620_Gpio_AddBlock(&pwm2); // gpio_block_3
     Mt3620_Gpio_AddBlock(&grp3); // gpio_block_4 
-/*
+
     Mt3620_Gpio_AddBlock(&gpio_block_5);
     Mt3620_Gpio_AddBlock(&gpio_block_6);
     Mt3620_Gpio_AddBlock(&gpio_block_7);
@@ -53,5 +53,5 @@ void init(void)
     Mt3620_Gpio_AddBlock(&gpio_block_9);
     Mt3620_Gpio_AddBlock(&gpio_block_A);
     Mt3620_Gpio_AddBlock(&gpio_block_B);
-*/  
+  
 }
